722dc1307c52091cde849ba4ff9625bde3e2715c
ishunegi <ishikanegi76@gmail.com>
2025-06-01 13:01:47 +0530

    const_let_var

---
+++ second.js
@@ -0,0 +1,14 @@
+const accountId = 1234
+let accountEmail = "ishika@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState
+
+accountEmail = "i123@gmail.com"
+accountPassword = "456"
+accountCity = "Bengaluru"
+
+//use let and const
+console.log(accountEmail)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
