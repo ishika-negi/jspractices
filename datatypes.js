4957f5520a7adee1f8a895d253860376ce875b3f
ishunegi <ishikanegi76@gmail.com>
2025-06-01 13:21:31 +0530

    second

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+"use strict";//trat all js code as a newer version
+//alert(3+3) We are using nodejs, not browser
+
+let name = "ISHIKA"
+let age = 19
+let isLoggedIn = false
+
+console.log(name)
+console.log(age)
+console.log(isLoggedIn)
+console.log(typeof name)
--- second.js
+++ second.js
@@ -8,7 +8,9 @@ accountEmail = "i123@gmail.com"
 accountPassword = "456"
 accountCity = "Bengaluru"
 
-//use let and const
+// use let and const and
+// prefer not to use
+// var because of issue in block scope and functional scope
 console.log(accountEmail)
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
