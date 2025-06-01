7c8799525c7a476cf0d2084057a93fcf7343c42a
ishunegi <ishikanegi76@gmail.com>
2025-06-01 18:30:24 +0530

    typeconversion

---
+++ typeconversion.js
@@ -0,0 +1,16 @@
+let score="24"
+console.log(typeof(score))
+console.log(score)
+
+let conversion = Number(score)
+console.log(typeof (conversion))
+console.log(conversion)
+
+let value=true
+console.log(typeof(value))
+
+let newconversion = Number(value)
+console.log(typeof(newconversion))
+console.log(newconversion)
+
+
