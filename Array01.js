7b318ff7d83f4b7472793bb992dceb18d6d3be4c
ishunegi <ishikanegi76@gmail.com>
2025-06-05 10:35:08 +0530

    array01

---
+++ Array01.js
@@ -0,0 +1,14 @@
+const arr=[10,20,30,40]//arrays are resizable
+console.log(arr[0])
+arr.push(50)
+console.log(arr)
+
+arr.unshift(0)
+console.log(arr)
+
+arr.unshift(9)
+console.log(arr)
+
+//array-copy operations crate shallow copies
+const name1=["ROHAN","LATTU","JOON"]
+const name2=["ISHIKA","ISHU","ISHI"]
