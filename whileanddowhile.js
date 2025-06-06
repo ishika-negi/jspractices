875573eec704af06fc1cd673460cff2bfc2ac405
ishunegi <ishikanegi76@gmail.com>
2025-06-06 16:29:23 +0530

    initial commit

---
+++ whileanddowhile.js
@@ -0,0 +1,18 @@
+let index = 0
+while(index <= 10){
+    console.log(index)
+    index+=2
+}
+
+let arr=["ironman", "hulk", "captain america", "black panther"]
+let first=0
+while(first<arr.length){
+    console.log(arr[first])
+    first++
+}
+
+let score = 1
+do{
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10)
